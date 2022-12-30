02e49141ba310534a8f91582445e3f4b1c8406f6
Ümit Tunç <umitreva@gmail.com>
2022-12-30 09:43:09 +0300

    beautify code

--- project/truncgil_server/olimpiyat/manager/resources/views/admin-ajax/tumunu-guncelle.blade.php
+++ project/truncgil_server/olimpiyat/manager/resources/views/admin-ajax/tumunu-guncelle.blade.php
@@ -6,31 +6,30 @@ $db = db("komut_istemi")
 ->get();
 $dizi = [];
 foreach($db AS $d) {
-    $d->imei = trim($d->imei);
-    $d->json = trim($d->json);
-    $dizi[$d->id]['info'] = $d;
-    $dizi[$d->id]['command'] = $d->json;
-    $params = ("imei={$d->imei}&command={$d->json}");
-    $data = [
-        'imei'=>$d->imei,
-        'command' => $d->json
-    ];
-    $return =  httpClient("http://app.olimpiyat.com.tr/client.php",$data);
-    $dizi[$d->id]['sonuc'] = $return;
-    $hex = substr(trim($return),$d->bas,$d->son - $d->bas);
- //   dump($d->title);
- //   dump($hex);
-    $decimal = intval($hex,16);
- //   dump($decimal);
-    $dizi[$d->id]['decimal'] = $decimal;
+         $d->imei = trim($d->imei);
+         $d->json = trim($d->json);
+         $dizi[$d->id]['info'] = $d;
+         $dizi[$d->id]['command'] = $d->json;
+         $params = ("imei={$d->imei}&command={$d->json}");
+         $data = [
+            'imei'=>$d->imei,
+            'command' => $d->json
+         ];
+         $return =  httpClient("http://app.olimpiyat.com.tr/client.php",$data);
+         $dizi[$d->id]['sonuc'] = $return;
+         $hex = substr(trim($return),$d->bas,$d->son - $d->bas);
+         //   dump($d->title);
+         //   dump($hex);
+         $decimal = intval($hex,16);
+         //   dump($decimal);
+         $dizi[$d->id]['decimal'] = $decimal;
 
-     
-    $_GET['imei'] = $d->imei;
-    $_GET['command'] = $d->json;
-    $_GET['sonuc'] = $return;
-    sleep(3);
+
+         $_GET['imei'] = $d->imei;
+         $_GET['command'] = $d->json;
+         $_GET['sonuc'] = $return;
+         sleep(3);
      ?>
-
      @include("admin-ajax.komut-son-durum-guncelle")
      <?php 
 }
