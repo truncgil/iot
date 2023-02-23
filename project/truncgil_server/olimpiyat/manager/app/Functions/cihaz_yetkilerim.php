869b9bb2cfae537a91a3e34846b75bb0410e7ecb
Ümit Tunç <umitreva@gmail.com>
2023-02-23 18:45:10 +0300

    device permissions added

--- project/truncgil_server/olimpiyat/manager/app/Functions/cihaz_yetkilerim.php
+++ project/truncgil_server/olimpiyat/manager/app/Functions/cihaz_yetkilerim.php
@@ -1,6 +1,8 @@
 <?php function cihaz_yetkilerim() {
     $u = u();
-    return db("yetkiler")->where("alias",$u->alias)
-    ->groupBy("imei")
-    ->pluck("imei")->toArray();
+    return db("yetkiler")
+        ->orWhere("alias",$u->alias)
+        ->orWhere("alias",$u->id)
+        ->groupBy("imei")
+        ->pluck("imei")->toArray();
 } ?>
